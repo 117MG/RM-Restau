57b39d1898a3aaba70ca59a168892e9960adf4b5
117MG <117.mayankgupta@gmail.com>
2021-07-25 18:34:56 +0530

    project added

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+//import { AngularFire, AuthProviders, AuthMethods } from '@angular/fire'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
