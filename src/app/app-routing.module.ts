75e9e3909a2c680143d28c092755dec1ee5c5fb0
117MG <117.mayankgupta@gmail.com>
2021-07-26 18:17:56 +0530

    Simple Edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,7 @@ import { UserinfoComponent } from './components/userinfo/userinfo.component';
 
 const routes: Routes = [
 
-{path:'', 
-redirectTo:'/login', 
-pathMatch:'full'},
+{path:'', redirectTo:'/login', pathMatch:'full'},
 
 {path:'login',
 component: LoginComponent},
